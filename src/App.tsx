08724fcf2c6618df0261adf92e3f290620dc33bf
tenma <ten.ten.tenma.goal@gmail.com>
2025-10-05 01:42:57 +0900

    タブ切替時のロジックを修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import ItemPage from "./pages/Item";
 import ChatPage from "./pages/Chat";
+import Chats from "./pages/Chats";
+import SoldItem from "./pages/SoldItem";
+import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      {/* ヘッダーとフッターを常に表示するページ */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="chats" element={<Chats />} />
+        <Route path="sold-items" element={<SoldItem />} />
+      </Route>
+
+      {/* 独立したページ */}
       <Route path="/login" element={<Login />} />
       <Route path="/item/:item_id" element={<ItemPage />} />
       <Route path="/chat/:item_id" element={<ChatPage />} />
