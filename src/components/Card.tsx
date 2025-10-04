935da04b1136352d1bbe2cefeb156a49ea9ee7c0
中田　進之介 <shin20040507@icloud.com>
2025-10-04 11:47:31 +0900

    表示される情報を追加

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,5 @@
-import { useNavigate } from 'react-router-dom';
-
-type Item = {
-  item_name: string;
-  // 必要に応じて他のプロパティも追加してください
-};
+import { useNavigate } from "react-router-dom";
+import type { Item } from "../types/item";
 
 function Card({ item }: { item: Item }) {
   const navigate = useNavigate();
@@ -11,9 +7,12 @@ function Card({ item }: { item: Item }) {
   return (
     <div className="border p-4">
       <h2 className="mb-2">{item.item_name}</h2>
+      <img src={item.item_img} alt={item.item_name} />
+      <p>{item.item_detail}</p>
+      <p>{item.item_price}</p>
       <button
         className="bg-gray-500 text-white p-1"
-        onClick={() => navigate('/item')}
+        onClick={() => navigate("/item")}
       >
         Itemページ
       </button>
