2923fa3be82039e98c9502b5e932dd6b09c6e670
tenma <ten.ten.tenma.goal@gmail.com>
2025-10-05 15:14:01 +0900

    商品詳細ページの実装

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -3,12 +3,14 @@ import type { Item } from "../types/item";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
+import RabbitButton from "../components/RabbitButton";
 
 function ItemPage() {
   const navigate = useNavigate();
   const { item_id: itemIdParam } = useParams();
   const [item, setItem] = useState<Item | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [isRabbitLiked, setIsRabbitLiked] = useState(false)
 
   useEffect(() => {
     const fetchItem = async () => {
@@ -48,6 +50,10 @@ function ItemPage() {
     );
   }
 
+  const handleRabbitClick = () => {
+    setIsRabbitLiked(!isRabbitLiked)
+    console.log(`スペシャルいいね${isRabbitLiked ? '解除' : '追加'}されました！`)
+  }
   // アイテムが存在しない場合
   if (!item) {
     return (
@@ -64,30 +70,41 @@ function ItemPage() {
   }
 
   return (
-    <>
-      <div className="p-4">
-        <h1 className="text-xl mb-4">Itemページ</h1>
-        <div>ここにはアイテムの詳細を配置</div>
-        <div>{item.item_name}</div>
-        <div>{item.item_detail}</div>
-        <div>¥{item.item_price}</div>
-        <div>
-          <img src={item.item_img} alt={item.item_name} />
+      <>
+        <div className="w-full h-80 border border-gray-200 rounded-md">
+          <img src={item.item_img} alt={item.item_name} className="w-full h-full object-cover" />
+        </div>
+        <div className="flex flex-col gap-2 pt-3">
+          <p className="text-lg text-gray-800 font-medium">{item.item_name}</p>
+          <div className="flex flex-row justify-between">
+            <div className="flex items-center gap-1">
+              <span className="text-sm text-black">¥</span>
+              <span className="text-4xl text-black-900 font-extrabold">{item.item_price}</span>
+            </div>
+            <RabbitButton 
+              className="px-5"
+              onClick={handleRabbitClick} 
+              isActive={isRabbitLiked}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 pt-7 pb-15">
+          <h1 className="font-bold">商品情報</h1>
+          <p className="text-base">{item.item_detail}</p>
         </div>
         <button
-          className="bg-blue-500 text-white p-2 mr-2"
+          className="bg-blue-500 text-white p-2 mr-2 border border-gray-200 rounded-md"
           onClick={() => navigate("/")}
         >
           Homeページ
         </button>
         <button
-          className="bg-orange-500 text-white p-2 mr-2"
+          className="bg-orange-500 text-white p-2 mr-2 border border-gray-200 rounded-md"
           onClick={() => navigate("/chat/" + item.item_id)}
         >
           Chatページ
         </button>
-      </div>
-    </>
+      </>
   );
 }
 export default ItemPage;
